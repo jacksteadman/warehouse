3bc0cd34389bcf205fc4512228a34b9ae22c5459
Jack Steadman <jsteadman@bluestatedigital.com>
2015-09-07 16:16:02 -0400

    edit

--- lib/db.php
+++ lib/db.php
@@ -25,13 +25,7 @@ $DB_CONFIGS['DM_DB_CREDS'] = array(
         'db_ssl' => true,
         'db_ssl_ca' => '/etc/pki/tls/certs/amazon-rds-2015.pem',
 );
-$DB_CONFIGS['DM_DB_CREDS'] = array(
-        'db_host' => '127.0.0.1',
-        'db_port' => '3306',
-        'db_name' => 'data_mart',
-        'db_user' => 'root',
-        'db_pass' => 'jms0882',
-);
+
 
 
 function get_db_credentials($client, $db = DEFAULT_DB_BUNDLE) {
